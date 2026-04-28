b1bc60ab9af66a07d3052a352d8325b6c333e6d3
Yang Zhang <zhangyang@fpllefia.com>
2026-04-28 14:41:54 +0000

    new pag

--- index.js
+++
@@ -1,6 +0,0 @@
-const btn = document.getElementById("toggleBtn");
-const barra = document.getElementById("barra");
-
-btn.addEventListener("click", () => {
-    barra.classList.toggle("activa");
-});
