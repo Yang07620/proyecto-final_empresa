2219583e7e440bcf9a8a7baa5f17a578ca56b9fa
Yang Zhang <zhangyang@fpllefia.com>
2026-04-29 11:15:51 +0000

    kkkkkk

---
+++ supabase.js
@@ -0,0 +1,4 @@
+const client = window.supabase.createClient(
+            "https://ienbdthevfgjglymjgfa.supabase.co",
+            "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImllbmJkdGhldmZnamdseW1qZ2ZhIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzczNzg0MzEsImV4cCI6MjA5Mjk1NDQzMX0.O1Vhd6F_okqCmm-lKvajJEz0dcUel1DKdw5wWEEKS_M"
+)
